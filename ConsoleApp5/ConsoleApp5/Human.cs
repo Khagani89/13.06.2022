6843fae27ca0cfbde11ccf40c22d763f9e06bed4
Khagani89 <khaganichh@code.edu.az>
2022-06-14 11:28:28 +0400

    project

---
+++ ConsoleApp5/ConsoleApp5/Human.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    internal class Human
+    {
+        public Human(byte age, string name = null, string surname = null)
+        {
+            this.Age = age;
+            this.Name = name;
+            this.Surname = surname;
+        }
+
+
+
+        public string Name;
+        public string Surname;
+        public byte Age;
+
+
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Human[] humans = new Human[]
+            {
+            new Human(10,"Abbas","Aliyev"),
+            new Human(20,"Rafet","Mammadov"),
+            new Human(30,"Rasim","Kazimov"),
+            new Human(40,"Elekber","Veliyev"),
+
+            };
+            Console.WriteLine("Axtaris deyerini daxil edin");
+            string search = Console.ReadLine();
+            
+            foreach (var item in humans) 
+            {
+                Console.WriteLine($"{item.Name} {item.Surname}  {item.Age}");
+            }
+
+        }
+    }
+}
